04ba29f2071e3498d0e99eae4680d9d1db7acaae
yassineprojs <bourawiaajina12@gmail.com>
2024-04-27 22:33:04 +0100

    completed the camera movement

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { Canvas, useFrame, extend, useThree } from "@react-three/fiber";
 import Experience from "./Experience.jsx";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, createContext, useContext, useRef, useState } from "react";
 import Mascott from "./components/Mascott.jsx";
 import { Cube } from "./components/Cubes.jsx";
 import Instructs from "./components/gameInstucts.jsx";
@@ -8,64 +8,70 @@ import { Character } from "./components/Character.jsx";
 import {
   Center,
   FirstPersonControls,
+  Html,
   OrbitControls,
   PerspectiveCamera,
 } from "@react-three/drei";
 import gsap from "gsap";
 
+// Create a Camera Context
+const CameraContext = createContext();
+
+// Custom Hook to manage camera animations
+const useCameraAnimations = (camera, positions) => {
+  const [currentPositionIndex, setCurrentPositionIndex] = useState(0);
+
+  const animateCamera = (newPosition) => {
+    gsap.to(camera.position, { ...newPosition, duration: 2.3 });
+  };
+
+  const goToNextPosition = () => {
+    const nextPositionIndex = (currentPositionIndex + 1) % positions.length;
+    animateCamera(positions[nextPositionIndex]);
+    setCurrentPositionIndex(nextPositionIndex);
+  };
+
+  const goToPreviousPosition = () => {
+    const prevPositionIndex =
+      (currentPositionIndex - 1 + positions.length) % positions.length;
+    animateCamera(positions[prevPositionIndex]);
+    setCurrentPositionIndex(prevPositionIndex);
+  };
+
+  return { goToNextPosition, goToPreviousPosition };
+};
+
 function CustomFirstPersonControls(props) {
-  const controls = useRef();
+  const controlsRef = useRef();
+
   const {
     camera,
     gl: { domElement },
   } = useThree();
-  const [currentCamPosition, setCurrentCamPosition] = useState(0);
-  const cameraPositions = [
-    { x: -0.446, y: -0.342, z: 0.032 },
-    { x: 0.95, y: -0.72, z: 0.665 },
-    { x: 0.82, y: -0.37, z: 1.67 },
-    { x: 0.906, y: 0.00023, z: 0.237 },
-    { x: 0.851, y: -0.217, z: -0.03 },
-  ];
-  const cameraRotations = [
-    { x: -0.494, y: -0.893, z: -0.397 },
-    { x: -2.44, y: -1.293, z: -2.465 },
-    { x: -3.06, y: -0.55, z: -3.099 },
-    { x: 0.29, y: -0.66, z: -0.183 },
-    { x: -1.39, y: -0.64, z: -1.28 },
-  ];
 
-  useEffect(() => {
-    const onMouseUp = () => {
-      const newPosition = cameraPositions[currentCamPosition];
-      const newRotation = cameraRotations[currentCamPosition];
+  // Provide the camera to the context
+  const setCamera = useContext(CameraContext);
+  useEffect(() => setCamera(camera), [camera, setCamera]);
 
-      if (newPosition && newRotation) {
-        gsap.to(camera.position, { ...newPosition, duration: 2.3 });
-        gsap.to(camera.rotation, { ...newRotation, duration: 2.3 });
-        setCurrentCamPosition(
-          (prevPosition) => (prevPosition + 1) % cameraPositions.length
-        );
-      }
-    };
-
-    domElement.addEventListener("mouseup", onMouseUp);
-    return () => {
-      domElement.removeEventListener("mouseup", onMouseUp);
-    };
-  }, [camera, currentCamPosition, domElement]);
+  // const cameraRotations = [
+  //   { x: -0.494, y: -0.893, z: -0.397 },
+  //   { x: -2.44, y: -1.293, z: -2.465 },
+  //   { x: -3.06, y: -0.55, z: -3.099 },
+  //   { x: 0.29, y: -0.66, z: -0.183 },
+  //   { x: -1.39, y: -0.64, z: -1.28 },
+  // ];
 
   const moveState = useRef({ moving: false, lastMoveTime: 0 });
   const dampingFactor = 0.01;
-  const timeout = 250; // Time in ms after which the rotation stops
+  const timeout = 100; // Time in ms after which the rotation stops
 
   useFrame(() => {
-    if (controls.current) {
+    if (controlsRef.current) {
       const timeSinceLastMove = Date.now() - moveState.current.lastMoveTime;
       if (moveState.current.moving && timeSinceLastMove > timeout) {
-        controls.current.lookSpeed *= 1 - dampingFactor;
-        if (controls.current.lookSpeed < 0.001) {
-          controls.current.lookSpeed = 0;
+        controlsRef.current.lookSpeed *= 1 - dampingFactor;
+        if (controlsRef.current.lookSpeed < 0.001) {
+          controlsRef.current.lookSpeed = 0;
           moveState.current.moving = false;
         }
       }
@@ -74,8 +80,8 @@ function CustomFirstPersonControls(props) {
 
   useEffect(() => {
     const handleMouseMove = () => {
-      if (controls.current) {
-        controls.current.lookSpeed = props.lookSpeed;
+      if (controlsRef.current) {
+        controlsRef.current.lookSpeed = props.lookSpeed;
         moveState.current.moving = true;
         moveState.current.lastMoveTime = Date.now();
       }
@@ -89,15 +95,32 @@ function CustomFirstPersonControls(props) {
   }, [props.lookSpeed, domElement]);
 
   return (
-    <FirstPersonControls
-      ref={controls}
-      args={[camera, domElement]}
-      {...props}
-    />
+    <>
+      <FirstPersonControls
+        ref={controlsRef}
+        args={[camera, domElement]}
+        {...props}
+      />
+    </>
   );
 }
 
 export default function Main(props) {
+  // for cam animation
+  const cameraPositions = [
+    { x: -0.942, y: 0, z: 0.41 },
+    { x: -0.446, y: -0.342, z: 0.032 },
+    { x: 0.95, y: -0.72, z: 0.665 },
+    { x: 0.82, y: -0.37, z: 1.67 },
+    { x: 0.906, y: 0.00023, z: 0.237 },
+    { x: 0.851, y: -0.217, z: -0.03 },
+  ];
+  const [camera, setCamera] = useState();
+  const { goToNextPosition, goToPreviousPosition } = useCameraAnimations(
+    camera,
+    cameraPositions
+  );
+
   const script = [
     {
       id: 1,
@@ -185,37 +208,60 @@ export default function Main(props) {
 
   return (
     <>
-      <Canvas
-        camera={{
-          fov: 60,
-          near: 0.1,
-          far: 200,
-          position: [-0.942, -0.5, 0.41],
-          // rotation: [1.125, -1.2, -1.107],
-        }}
-      >
-        {/* <OrbitControls /> */}
+      <CameraContext.Provider value={setCamera}>
+        <Canvas
+          camera={{
+            fov: 60,
+            near: 0.1,
+            far: 200,
+            position: [-0.942, 0, 0.41],
+          }}
+        >
+          {/* <OrbitControls /> */}
 
-        <CustomFirstPersonControls
-          lookSpeed={0.05}
-          movementSpeed={0}
-          noFly={true}
-          activeLook={true}
-          autoForward={false}
-        />
-        <Center>
-          <color attach="background" args={["#DFF5FF"]} />
-          <Experience
-            onMeshClick={() => setIsOverlayVisible(true)}
-            onHintClick={handleHintClick}
-            visible={isCubeVisible}
-            gameWon={isGameWon}
+          <CustomFirstPersonControls
+            lookSpeed={0.01}
+            movementSpeed={0}
+            noFly={true}
+            activeLook={true}
+            autoForward={false}
           />
 
-          <Cube visible={isCubeVisible} onAllWordsCompleted={setIsGameWon} />
-        </Center>
-      </Canvas>
+          <Center>
+            <color attach="background" args={["#DFF5FF"]} />
+            <Experience
+              onMeshClick={() => setIsOverlayVisible(true)}
+              onHintClick={handleHintClick}
+              visible={isCubeVisible}
+              gameWon={isGameWon}
+            />
 
+            <Cube visible={isCubeVisible} onAllWordsCompleted={setIsGameWon} />
+          </Center>
+        </Canvas>
+
+        <div
+          style={{
+            position: "absolute",
+            width: "95%",
+
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%,50%)",
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            pointerEvents: "none",
+          }}
+        >
+          <button onClick={goToPreviousPosition} className="cam_btn">
+            <ion-icon name="caret-back-outline"></ion-icon>
+          </button>
+          <button onClick={goToNextPosition} className="cam_btn">
+            <ion-icon name="caret-forward-outline"></ion-icon>
+          </button>
+        </div>
+      </CameraContext.Provider>
       {boxCubeVisible && (
         <div
           style={{
